fc9f6fac9909ff49003ec9fc8584cc2becb8a311
Fabio Tust <fabiotust@MacBook-Pro-de-Fabio.local>
2022-12-20 10:31:35 -0300

    Adicionando o modalmapper para facilitar o mapeamento

--- src/main/java/pdev/com/agenda/api/mapper/PacienteMapper.java
+++ src/main/java/pdev/com/agenda/api/mapper/PacienteMapper.java
@@ -1,2 +1,59 @@
-package pdev.com.agenda.api.mapper;public class PacienteMapper {
+package pdev.com.agenda.api.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import pdev.com.agenda.api.request.PacienteRequest;
+import pdev.com.agenda.api.response.PacienteResponse;
+import pdev.com.agenda.domain.entity.Paciente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component // preciso colocar como um component agroa que estou usando model mapper
+@RequiredArgsConstructor // preciso dessa ajuda do lombok para usar o modelmapper
+public class PacienteMapper {
+
+
+    private final ModelMapper mapper;
+
+    public Paciente toPaciente{
+
+    }
+
+//    public static Paciente toPaciente(PacienteRequest request) {
+//        Paciente paciente = new Paciente();
+//        paciente.setNome(request.getNome());
+//        paciente.setSobrenome(request.getSobrenome());
+//        paciente.setEmail(request.getEmail());
+//        paciente.setCpf(request.getCpf());
+//
+//        return paciente;
+//
+//    }
+
+    public static PacienteResponse toPacienteResponse(Paciente paciente) {
+        PacienteResponse response = new PacienteResponse();
+        response.setId(paciente.getId());
+        response.setNome(paciente.getNome());
+        response.setSobrenome(paciente.getSobrenome());
+        response.setEmail(paciente.getEmail());
+        response.setCpf(paciente.getCpf());
+
+        return response;
+
+    }
+
+    public static List<PacienteResponse> toPacienteResponseList(List<Paciente> pacientes) {
+        // crio lista vazio
+        List<PacienteResponse> responses = new ArrayList<>();
+        // itero para adicionar a nova lista os pacientes response
+        for (Paciente paciente : pacientes){
+            //usando o proprio metodo de response
+            responses.add(toPacienteResponse(paciente));
+        }
+
+        return responses;
+
+    }
 }
--- src/main/java/pdev/com/agenda/config/ModalMapperConfig.java
+++ src/main/java/pdev/com/agenda/config/ModalMapperConfig.java
@@ -1,2 +1,14 @@
-package pdev.com.agenda.config;public class ModalMapperConfig {
+package pdev.com.agenda.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModalMapperConfig {
+    // com o bean o spring ja sabe como trabalhar com essa config e fazer a injecao de dependencias
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
